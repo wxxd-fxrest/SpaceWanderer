6665d928f419f9d9b241a46361452e758a61cc29
wxxd-fxrest <fxrest704@gmail.com>
2024-10-18 19:43:18 +0900

    feat(front)/ creat project

---
+++ SpaceWanderer/SpaceWanderer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceWanderer
+//
+//  Created by 밀가루 on 10/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
