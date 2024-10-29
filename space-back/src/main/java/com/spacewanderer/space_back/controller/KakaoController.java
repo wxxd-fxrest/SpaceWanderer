d70e6e4beafa65c7b0fed741ea7d3a6de8a5ab8c
wxxd-fxrest <fxrest704@gmail.com>
2024-10-29 19:15:10 +0900

    feat(kakao): file 생성

--- space-back/src/main/java/com/spacewanderer/space_back/controller/KakaoController.java
+++ space-back/src/main/java/com/spacewanderer/space_back/controller/KakaoController.java
@@ -1,5 +1,21 @@
 package com.spacewanderer.space_back.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spacewanderer.space_back.entity.UserEntity;
+import com.spacewanderer.space_back.service.KakaoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth/oauth2")
+@RequiredArgsConstructor
 public class KakaoController {
-    
+    private final KakaoService kakaoService;
+
 }
--- space-back/src/main/java/com/spacewanderer/space_back/service/KakaoService.java
+++ space-back/src/main/java/com/spacewanderer/space_back/service/KakaoService.java
@@ -1,5 +1,30 @@
 package com.spacewanderer.space_back.service;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.spacewanderer.space_back.dto.KakaoUserDTO;
+import com.spacewanderer.space_back.entity.UserEntity;
+import com.spacewanderer.space_back.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class KakaoService {
-    
+    @Value("${KAKAO.REDIRECT.URL}")
+    private String kakaoRedirectUrl;
+    // REST API Key 주입
+    @Value("${KAKAO.REST.API.KEY}")
+    private String kakaoApiKey;
+
 }
